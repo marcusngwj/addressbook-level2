f78863a62790392446a387387d2982bfad2aa0e7
“marcusngwj” <marcusngwj@msn.com>
2016-08-26 22:34:18 +0800

    Add Street validation

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -1,14 +1,34 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 /**
- * Represents a Person's street number in the address book.
+ * Represents a Person's street name in the address book.
  * Guarantees: immutable.
  */
 public class Street {
+    public static final String MESSAGE_STREET_CONSTRAINTS = "";
+    public static final String STREET_VALIDATION_REGEX = "";
+    
     public final String value;
     
-    public Street(String street){
+    /**
+     * Validates given street.
+     *
+     * @throws IllegalValueException if given street string is invalid.
+     */
+    public Street(String street) throws IllegalValueException {
+        if (!isValidStreet(street)) {
+            throw new IllegalValueException(MESSAGE_STREET_CONSTRAINTS);
+        }
         this.value = street;
+    }
+    
+    /**
+     * Returns true if a given string is a valid street name.
+     */
+    public static boolean isValidStreet(String test) {
+        return test.matches(STREET_VALIDATION_REGEX);
     }
 
     @Override
