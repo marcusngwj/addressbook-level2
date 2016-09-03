eab584e96a2495f184ffcb9b364e25499576fe76
“marcusngwj” <marcusngwj@msn.com>
2016-09-03 11:27:36 +0800

    Add default method getPrintableString(Printable... printables)

--- src/seedu/addressbook/data/person/ReadOnlyPerson.java
+++ src/seedu/addressbook/data/person/ReadOnlyPerson.java
@@ -14,6 +14,11 @@ public interface ReadOnlyPerson {
     Email getEmail();
     Address getAddress();
 
+    int NAME = 0;
+    int PHONE = 1;
+    int EMAIL = 2;
+    int ADDRESS = 3;
+
     /**
      * The returned TagList is a deep copy of the internal TagList,
      * changes on the returned list will not affect the person's internal tags.
@@ -32,49 +37,22 @@ public interface ReadOnlyPerson {
                 && other.getAddress().equals(this.getAddress()));
     }
 
-    /**
-     * Formats the person as text, showing all contact details.
-     */
-    default String getAsTextShowAll() {
-        final StringBuilder builder = new StringBuilder();
-        final String detailIsPrivate = "(private) ";
-        builder.append(getName())
-                .append(" Phone: ");
-        if (getPhone().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getPhone())
-                .append(" Email: ");
-        if (getEmail().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getEmail())
-                .append(" Address: ");
-        if (getAddress().isPrivate()) {
-            builder.append(detailIsPrivate);
-        }
-        builder.append(getAddress())
-                .append(" Tags: ");
-        for (Tag tag : getTags()) {
-            builder.append(tag);
-        }
-        return builder.toString();
-    }
+    
 
     /**
-     * Formats a person as text, showing only non-private contact details.
+     * Returns a concatenated version of the printable strings of each object.
      */
-    default String getAsTextHidePrivate() {
+    default String getPrintableString(Printable... printables) {
         final StringBuilder builder = new StringBuilder();
-        builder.append(getName());
+        builder.append("Name: ").append(printables[NAME]);
         if (!getPhone().isPrivate()) {
-            builder.append(" Phone: ").append(getPhone());
+            builder.append(", Phone: ").append(printables[PHONE]);
         }
         if (!getEmail().isPrivate()) {
-            builder.append(" Email: ").append(getEmail());
+            builder.append(", Email: ").append(printables[EMAIL]);
         }
         if (!getAddress().isPrivate()) {
-            builder.append(" Address: ").append(getAddress());
+            builder.append(", Address: ").append(printables[ADDRESS]);
         }
         builder.append(" Tags: ");
         for (Tag tag : getTags()) {
