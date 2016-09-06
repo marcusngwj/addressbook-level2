fcfe7cc929a0898f57b6ab35961428eb0ddef10f
“marcusngwj” <marcusngwj@msn.com>
2016-09-06 16:33:57 +0800

    Update JUnit test for isSimilar() for different ordering of a name

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -14,5 +14,12 @@ public class NameTest {
         Name newName = new Name("gEralD hubErT iRviN");
         assertTrue(existingName.isSimilar(newName));
     }
+    
+    @Test
+    public void isSimilar_differentOrder() throws IllegalValueException{
+        Name existingName = new Name("Gerald Hubert Irvin");
+        Name newName = new Name("Hubert Irvin Gerald");
+        assertTrue(existingName.isSimilar(newName));
+    }
 
 }
