cdb17b0f806b991c7c5a74a69e5106203647ec2a
“marcusngwj” <marcusngwj@msn.com>
2016-09-06 16:36:13 +0800

    Update JUnit test for isSimilar for part of name

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -21,5 +21,18 @@ public class NameTest {
         Name newName = new Name("Hubert Irvin Gerald");
         assertTrue(existingName.isSimilar(newName));
     }
+    
+    @Test
+    public void isSimilar_containsPartOfName() throws IllegalValueException{
+        Name existingName = new Name("Gerald Hubert Irvin");
+        Name namePart1 = new Name("Gerald");
+        assertTrue(existingName.isSimilar(namePart1));
+        
+        Name namePart2 = new Name("Hubert");
+        assertTrue(existingName.isSimilar(namePart2));
+        
+        Name namePart3 = new Name("Irvin");
+        assertTrue(existingName.isSimilar(namePart3));
+    }
 
 }
